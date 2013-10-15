a6d83f9f307a9109ff8790ece924da494fe219ce
Shalzz <shaleen.jain95@gmail.com>
2013-10-15 22:22:39 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Multi_Drive_Copy_Utiliy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.copyFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape2 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.refreshList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File to Copy:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(93, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // selectFile
+            // 
+            this.selectFile.Location = new System.Drawing.Point(428, 32);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(88, 23);
+            this.selectFile.TabIndex = 2;
+            this.selectFile.Text = "Select File";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(292, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(173, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // copyFile
+            // 
+            this.copyFile.Location = new System.Drawing.Point(159, 225);
+            this.copyFile.Name = "copyFile";
+            this.copyFile.Size = new System.Drawing.Size(171, 28);
+            this.copyFile.TabIndex = 4;
+            this.copyFile.Text = "Copy File to Drives";
+            this.copyFile.UseVisualStyleBackColor = true;
+            this.copyFile.Click += new System.EventHandler(this.copyFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape2,
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(521, 272);
+            this.shapeContainer1.TabIndex = 6;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape2
+            // 
+            this.rectangleShape2.Location = new System.Drawing.Point(25, 80);
+            this.rectangleShape2.Name = "rectangleShape2";
+            this.rectangleShape2.Size = new System.Drawing.Size(214, 126);
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Location = new System.Drawing.Point(255, 80);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(242, 127);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(278, 71);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Manual Selection";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(54, 72);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(119, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Automatic Selection";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(64, 108);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(112, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Removable Drives";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(64, 131);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(109, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Fixed Hard Drives";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(64, 154);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(70, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "CD Roms";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // refreshList
+            // 
+            this.refreshList.Image = global::Multi_Drive_Copy_Utiliy.Properties.Resources.refresh_black;
+            this.refreshList.Location = new System.Drawing.Point(468, 90);
+            this.refreshList.Name = "refreshList";
+            this.refreshList.Size = new System.Drawing.Size(24, 24);
+            this.refreshList.TabIndex = 5;
+            this.refreshList.UseVisualStyleBackColor = true;
+            this.refreshList.Click += new System.EventHandler(this.refreshList_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.copyFile;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(521, 272);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.refreshList);
+            this.Controls.Add(this.copyFile);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button copyFile;
+        private System.Windows.Forms.Button refreshList;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape2;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Multi_Drive_Copy_Utiliy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // click the refresh button on form initialization
+            copyFile.PerformClick();
+        }
+
+        private void selectFile_Click(object sender, EventArgs e)
+        {
+            // Open a openFileDialog and select a file
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void copyFile_Click(object sender, EventArgs e)
+        {
+            // Get the file name
+            String sourceFile = textBox1.Text;
+            int index = sourceFile.LastIndexOf("\\");
+            String fileName = sourceFile.Substring(index + 1);
+
+            // For Manual selection
+            if (radioButton1.Checked)
+            {
+                // Copy file into each Selected Drive
+                foreach (Object selecteditem in listBox1.SelectedItems)
+                {
+                    // Extract the Drive letter from ListBox Item
+                    String item = selecteditem as String;
+                    int itemIndex = item.LastIndexOf("\\");
+                    String driveLetter = item.Substring(0, itemIndex);
+                    // Form the complete address
+                    String destFile = driveLetter + "\\" + fileName;
+                    File.Copy(sourceFile, destFile, true);
+                }
+                MessageBox.Show("Copy Complete!");
+            }
+
+            // For automatic selection
+            if (radioButton2.Checked)
+            {
+                if (radioButton3.Checked)
+                {
+                    DriveInfo[] drives = DriveInfo.GetDrives();
+                    foreach (DriveInfo Drive in drives)
+                    {
+                        try
+                        {
+                            if (Drive.DriveType == DriveType.Removable)
+                                File.Copy(sourceFile, Drive.Name + "\\" + fileName, true);
+                        }
+                        catch(Exception ex)
+                        {
+                                MessageBox.Show(ex.Message);
+                        }
+                    }
+                    MessageBox.Show("Copy Complete!");
+                }
+                else if (radioButton4.Checked)
+                {
+                     DriveInfo[] drives = DriveInfo.GetDrives();
+                     foreach (DriveInfo Drive in drives)
+                    {
+                        try
+                        {
+                            if (Drive.DriveType == DriveType.Fixed)
+                                File.Copy(sourceFile, Drive.Name + "\\" + fileName, true);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    MessageBox.Show("Copy Complete!");
+                }
+                else if (radioButton5.Checked)
+                {
+                    DriveInfo[] drives = DriveInfo.GetDrives();
+                    foreach (DriveInfo Drive in drives)
+                    {
+                        try
+                        {
+                            if (Drive.DriveType == DriveType.CDRom)
+                                File.Copy(sourceFile, Drive.Name + "\\" + fileName, true);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    MessageBox.Show("Copy Complete!");
+                }
+                else
+                {
+                    MessageBox.Show("Please select an Option");
+                }
+            }
+        }
+
+        private void refreshList_Click(object sender, EventArgs e)
+        {
+            // Get a list of drives and display them
+            listBox1.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo Drive in drives)
+            {
+                listBox1.Items.Add(Drive.Name + "\tType: " + Drive.DriveType);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Change the UI
+            listBox1.Enabled = true;
+            refreshList.Enabled = true;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Change the UI
+            listBox1.Enabled = false;
+            refreshList.Enabled = false;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+            radioButton5.Enabled = true;
+        }
+       
+    }
+}
